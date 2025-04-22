d447bdcb63dd6c5de04740881c3023449a0fff60
elizave-taa <114254752+elizave-taa@users.noreply.github.com>
2025-04-23 00:18:29 +0300

    Connect Bootstrap Icons

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  get '*path', to: 'home#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
