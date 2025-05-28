18d9635948cbaf99b9ec1922eda2da5a25199f9f
elizave-taa <114254752+elizave-taa@users.noreply.github.com>
2025-05-29 00:57:05 +0300

    Add Project Page

---
+++ app/javascript/ProjectPage.vue
@@ -0,0 +1,43 @@
+<script>
+import ContentPlan from './ContentPlan.vue'
+import ContentPlanItem from "./ContentPlanItem.vue";
+import GeneralNavBar from "./GeneralNavBar.vue";
+export default {
+  name: "ProjectPage",
+  components: {GeneralNavBar, ContentPlanItem, ContentPlan},
+};
+</script>
+
+<template>
+  <div class="page-wrapper">
+    <general-nav-bar></general-nav-bar>
+    <div class="main-container">
+      <content-plan></content-plan>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.page-wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #e3c3d4;
+}
+
+.main-container {
+  flex: 1;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
